201885471f9816fa52b414bfb6ec4e8954fa811f
Vasiliki Gerardi <gerardivasiliki@gmail.com>
2025-04-05 23:47:01 +0100

    Add models and migrations

---
+++ checker/posts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=75)
+    body  = models.TextField()
+    slug  = models.SlugField()
+    date  = models.DateField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title 
